df01480e17773c76b4a243be30ae47d9495e1fd3
tjen5581 <69872448+tjen5581@users.noreply.github.com>
2021-05-15 08:09:44 -0400

    Update keypadlock_test.py

--- keypadlock_test.py
+++ keypadlock_test.py
@@ -4,164 +4,164 @@ from statemachine import StateMachine
 
 input = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-def t0_transitions(input):
+def j0_transitions(input):
     print("start state")
-    print("current state: q0\tinput: {}".format(input))
+    print("current state: j0\tinput: {}".format(input))
     if input == "1":
-        newState = "t1"
+        newState = "j1"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet:
-        newState = "t7"
+        newState = "j7"
     else:
         newState = "error_state"
     time.sleep(2)
     
     return newState, input
 
-def t1_transitions(input):
-    print("current state: q1\tinput: {}".format(input))
+def j1_transitions(input):
+    print("current state: j1\tinput: {}".format(input))
     if input == "4":
-        newState = "t2"
+        newState = "j2"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet:       
-        newState = "t8"
+        newState = "j8"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t2_transitions(input):
+def j2_transitions(input):
     print("current state: t2\tinput: {}".format(input))
     if input == "7":
-        newState = "t3"
+        newState = "j3"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet: 
-        newState = "t9"
+        newState = "j9"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t3_transitions(input):
-    print("current state: t3\jinput: {}".format(input))
+def j3_transitions(input):
+    print("current state: j3\tinput: {}".format(input))
     if input == "2":
-        newState = "t4"
+        newState = "j4"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet:  
-        newState = "t10"
+        newState = "j10"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t4_transitions(input):
-    print("current state: t4\jinput: {}".format(input))
+def j4_transitions(input):
+    print("current state: j4\tinput: {}".format(input))
     if input == "5":
-        newState = "t5"
+        newState = "j5"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet:   
-        newState = "t11"
+        newState = "j11"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t5_transitions(input):
-    print("current state: t5\tinput: {}".format(input))
+def j5_transitions(input):
+    print("current state: j5\tinput: {}".format(input))
     if input == "8":
         print("UNLOCKED")
         time.sleep(2)
-        newState = "t12"
+        newState = "j12"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     elif input in alphabet:  
         print("INCORRECT COMBINATION.")
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t7_transitions(input):
-    print("current state: t7\tinput: {}".format(input))
+def j7_transitions(input):
+    print("current state: j7\tinput: {}".format(input))
     if input in alphabet: 
-        newState = "t8"
+        newState = "j8"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t8_transitions(input):
-    print("current state: t8\jinput: {}".format(input))
+def j8_transitions(input):
+    print("current state: j8\tinput: {}".format(input))
     if input in alphabet: 
-        newState = "t9"
+        newState = "j9"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t9_transitions(input):
-    print("current state: t9\jinput: {}".format(input))
+def j9_transitions(input):
+    print("current state: j9\tinput: {}".format(input))
     if input in alphabet: 
-        newState = "t10"
+        newState = "j10"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t10_transitions(input):
-    print("current state: t10\jinput: {}".format(input))
+def j10_transitions(input):
+    print("current state: j10\tinput: {}".format(input))
     if input in alphabet: 
-        newState = "t11"
+        newState = "j11"
     elif input == "#":
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t11_transitions(input):
-    print("current state: t11\jinput: {}".format(input))
+def j11_transitions(input):
+    print("current state: j11\tinput: {}".format(input))
     if input in alphabet: 
         print("INCORRECT COMBINATION.")
-        newState = "t0"
+        newState = "j0"
     else:
         newState = "error_state"
     time.sleep(2)
     return newState, input
 
-def t12_transitions(input):
+def j12_transitions(input):
     print("current state: final state\tinput: {}".format(input))
     
 
 if __name__ == "__main__":
     m = StateMachine()
-    m.add_state("t0", t0_transitions)
-    m.add_state("t1", t1_transitions)
-    m.add_state("t2", t2_transitions)
-    m.add_state("t3", t3_transitions)
-    m.add_state("t4", t4_transitions)
-    m.add_state("t5", t5_transitions)
+    m.add_state("j0", j0_transitions)
+    m.add_state("j1", j1_transitions)
+    m.add_state("j2", j2_transitions)
+    m.add_state("j3", j3_transitions)
+    m.add_state("j4", j4_transitions)
+    m.add_state("j5", j5_transitions)
    
-  #m.add_state("t6", t6_transitions) #, end_state=1)
-    m.add_state("t7", t7_transitions)
-    m.add_state("t8", t8_transitions)
-    m.add_state("t9", t9_transitions)
-    m.add_state("t10", t10_transitions)
-    m.add_state("t11", t11_transitions)
-    m.add_state("t12", t12_transitions, end_state = 1)
-    m.set_start("t0")
+  #m.add_state("j6", j6_transitions) #, end_state=1)
+    m.add_state("j7", j7_transitions)
+    m.add_state("j8", j8_transitions)
+    m.add_state("j9", j9_transitions)
+    m.add_state("j10", j10_transitions)
+    m.add_state("j11", j11_transitions)
+    m.add_state("j12", j12_transitions, end_state = 1)
+    m.set_start("j0")
 
     #Test 1
     m.run("01#0010") 
